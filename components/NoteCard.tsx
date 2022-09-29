cbddbe04cacd1cecdd0481020c96212cf7958759
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-09-29 11:51:16 -0500

    style fix in NoteCard

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function NoteCard({ noteFrontMatter }) {
   return (
-    <div className="p-2 drop-shadow-lg bg-red-50 rounded-lg flex items-center justify-center shadow-red-400/50">
+    <div className="p-2 drop-shadow-lg bg-red-50 rounded-lg flex flex-col items-center justify-center shadow-red-400/50">
       <Image src={noteFrontMatter.cover} width="100" height="100" alt={noteFrontMatter.title}/>
       <span>{noteFrontMatter.title}</span>
     </div>
