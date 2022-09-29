ecffbccad2591e30d16c01f02dfc288b73c5d58b
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-09-29 11:30:48 -0500

    add the tailwind css plugin

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -1,6 +1,6 @@
 export default function NoteCard({ noteFrontMatter }){
     return (
-        <div>
+        <div className="p-2 drop-shadow-lg">
             <span>{noteFrontMatter.title}</span>
         </div>
     )
