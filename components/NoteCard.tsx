6f4f11d5c66f5006fd228c639eb187b06b56eee2
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-09-29 11:41:20 -0500

    test styles for NoteCard

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -1,7 +1,7 @@
-export default function NoteCard({ noteFrontMatter }){
-    return (
-        <div className="p-2 drop-shadow-lg">
-            <span>{noteFrontMatter.title}</span>
-        </div>
-    )
+export default function NoteCard({ noteFrontMatter }) {
+  return (
+    <div className="p-2 drop-shadow-lg bg-red-50 rounded-lg flex items-center justify-center shadow-red-400/50">
+      <span>{noteFrontMatter.title}</span>
+    </div>
+  );
 }
