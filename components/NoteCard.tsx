f50bddd69d4f11db1511a8f868992c948fe08afc
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-09-30 12:14:54 -0500

    styled the NoteCard a bit

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function NoteCard({ noteFrontMatter }) {
   return (
-    <div className="p-2 drop-shadow-lg bg-red-50 rounded-lg flex flex-col items-center justify-center shadow-red-400/50 m-2 h-40 w-40">
+    <div className="p-2 shadow-md hover:scale-105 duration-300 ease-in-out hover:cursor-pointer hover:shadow-lg hover:shadow-red-500/50 bg-red-50 rounded-lg flex flex-col items-center justify-center shadow-red-400/50 m-2 h-40 w-40">
       {noteFrontMatter.cover && (
         <div className="mb-2">
           <Image
