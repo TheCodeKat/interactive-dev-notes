1bf8715c63388a5d401f7a94758c3aabc0593290
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-10-01 18:54:51 -0500

    created the Header component

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-50 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 border border-gray-200 p-4 text-center">
+      <h2>soft dev notes</h2>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'rosewhite': '#fffdfd',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      }
+    }
+    
   },
   plugins: [
     plugin(function({ addBase, theme }) {
       addBase({
         'h1': { fontSize: theme('fontSize.4xl'), fontWeight: theme('fontWeight.black') },
         'h2': { fontSize: theme('fontSize.xl') },
-        'h3': { fontSize: theme('fontSize.lg') },
+        'h3': { fontSize: theme('fontSize.lg'), fontWeight: theme('fontWeight.bold') },
       })
     })
   ]
